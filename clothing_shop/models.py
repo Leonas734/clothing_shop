f2013185540d46164a4eec8a7dfcec40ad81dea4
Leonas734 <leonasadomaitis@gmail.com>
2021-10-08 17:38:31 +0100

    INITIALISED PROJECT

---
+++ clothing_shop/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.db.models.fields.related import ForeignKey
+from django.utils import timezone
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=100)
+    desc = models.TextField(max_length = 500)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    colour = models.CharField(max_length=30)
+    category = models.CharField(max_length=30, default='other')
+    dateOfUpload = models.DateField(default=timezone.now)
+    images = models.ImageField(upload_to='images/')
+    tags = models.CharField(max_length=500, default='male,female,')
+
+    def __str__(self):
+        return self.title
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    images = models.ImageField(upload_to = 'clothing_shop/images/')
+ 
+    def __str__(self):
+        return self.product.title
---
+++ clothing_shop/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.shortcuts import render
+
+from .models import Product, ProductImage
+
+
+def home_page_view(request):
+    product = Product.objects.get()
+    print(product)
+    productsImgs = ProductImage.objects.filter(product=product)
+    return render(request, 'clothing_shop/home_page.html', {
+        'product': product,
+        'productImgs': productsImgs
+    })
