862f95014271f6124b5c7bbf0e39e0775acab3e1
Leonas734 <leonasadomaitis@gmail.com>
2021-10-09 14:30:50 +0100

    Created navbar Navbar built using flexbox.

--- clothing_shop/views.py
+++ clothing_shop/views.py
@@ -6,7 +6,7 @@ from .models import Product, ProductImage
 
 def home_page_view(request):
     product = Product.objects.get()
-    print(product)
+    print(product.desc)
     productsImgs = ProductImage.objects.filter(product=product)
     return render(request, 'clothing_shop/home_page.html', {
         'product': product,
