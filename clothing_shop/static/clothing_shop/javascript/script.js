ad26233b7971580142f86d08b23efee62be3553f
Leonas734 <leonasadomaitis@gmail.com>
2021-10-12 11:28:00 +0100

    Created shop-outfit section. Javascript is used for product image sliding

--- clothing_shop/static/clothing_shop/javascript/script.js
+++ clothing_shop/static/clothing_shop/javascript/script.js
@@ -34,8 +34,69 @@ function addActiveClassNameToElement(element, className) {
   });
   element.classList.add(className);
 }
+
+function addImageSliderToProducts(parentEl) {
+  let products = [...parentEl.children];
+  products.forEach((productDiv) => {
+    const productDivChildElms = [...productDiv.children];
+    productDivChildElms.forEach((productEl) => {
+      if (!isImgContainer(productEl)) return;
+      const imgContainerDiv = productEl;
+      const productImgs = [...imgContainerDiv.getElementsByTagName("img")];
+      if (productImgs.length === 0) return;
+      offsetEachImage(productImgs);
+      addEvListenerToImgContainer(imgContainerDiv, productImgs);
+    });
+  });
+
+  function isImgContainer(element) {
+    const result = element.classList.value.includes("image-container")
+      ? true
+      : false;
+    return result;
+  }
+  function offsetEachImage(productImgs) {
+    productImgs.forEach((img, index) => {
+      img.style.left = `${100 * index}%`;
+    });
+  }
+  function addEvListenerToImgContainer(imgContainerDiv, productImgs) {
+    let currSlide = 0;
+
+    imgContainerDiv.addEventListener("click", function (e) {
+      const eventClassNames = e.target.classList.value;
+      const totalSlides = productImgs.length;
+
+      if (eventClassNames.includes("arrow-left")) {
+        currSlide += 1;
+      } else if (eventClassNames.includes("arrow-right")) {
+        currSlide -= 1;
+      } else {
+        return; // User clicked inside img container, but not either arrow.
+      }
+      // User reached end
+      if (currSlide === 1) {
+        currSlide = -totalSlides + 1;
+      }
+      if (currSlide === -totalSlides) {
+        currSlide = 0;
+      }
+      slideImages(productImgs, currSlide);
+    });
+  }
+  function slideImages(images, currSlide) {
+    images.forEach((img) => {
+      img.style.transform = `translateX(${currSlide * 100}%)`;
+    });
+  }
+}
+
 function init() {
   mainSectionEventHandler();
+  const womenProductsEl = document.querySelector(
+    ".shop-outfit__products-women"
+  );
+  addImageSliderToProducts(womenProductsEl);
 }
 
 init();
--- clothing_shop/views.py
+++ clothing_shop/views.py
@@ -5,10 +5,9 @@ from .models import Product, ProductImage
 
 
 def home_page_view(request):
-    product = Product.objects.get()
-    print(product.desc)
-    productsImgs = ProductImage.objects.filter(product=product)
-    return render(request, 'clothing_shop/home_page.html', {
-        'product': product,
-        'productImgs': productsImgs
-    })
+    allProducts = Product.objects.all()
+    products = dict()
+    for index, product in enumerate(allProducts):
+        productsImgs = ProductImage.objects.filter(product=product)
+        products[index] = {'product': product, 'productImgs': productsImgs}
+    return render(request, 'clothing_shop/home_page.html', {'products': products})
